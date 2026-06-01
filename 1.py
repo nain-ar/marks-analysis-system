05f7e3cc37791b98358bb3f7c5b1bd2e888884f6
nain-ar <arnain897@gmail.com>
2026-06-01 21:38:50 +0530

    clean up repository and remove extra files

--- 1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df=pd.read_csv(r"C:\Users\HP\Downloads\archive\ai_student_impact_dataset (1).csv")
-df.head(10)
-print(df)
--- 2.py
+++
@@ -1,5 +0,0 @@
-x= 75
-if x>40:
-    print ('u have passed!')
-else:
-    print('u have failed!')
